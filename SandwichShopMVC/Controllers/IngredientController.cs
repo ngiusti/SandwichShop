7606fe2c2e25dcf8dc8bf113593959841bd46a14
NashNWood <nashua.wood@gmail.com>
2017-07-06 12:20:13 -0700

    Added Ingredient and Menu Controllers

---
+++ SandwichShopMVC/Controllers/IngredientController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SandwichShopMVC.DAL;
+using SandwichShopMVC.Models;
+
+namespace SandwichShopMVC.Controllers
+{
+    public class IngredientController : Controller
+    {
+        private RestaurantContext db = new RestaurantContext();
+
+        // GET: Ingredient
+        public ActionResult Index()
+        {
+            return View(db.Ingredients.ToList());
+        }
+
+        // GET: Ingredient/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredients ingredients = db.Ingredients.Find(id);
+            if (ingredients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredients);
+        }
+
+        // GET: Ingredient/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Ingredient/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Cost,Quantity,Category")] Ingredients ingredients)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ingredients.Add(ingredients);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ingredients);
+        }
+
+        // GET: Ingredient/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredients ingredients = db.Ingredients.Find(id);
+            if (ingredients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredients);
+        }
+
+        // POST: Ingredient/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Cost,Quantity,Category")] Ingredients ingredients)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingredients).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ingredients);
+        }
+
+        // GET: Ingredient/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredients ingredients = db.Ingredients.Find(id);
+            if (ingredients == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredients);
+        }
+
+        // POST: Ingredient/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ingredients ingredients = db.Ingredients.Find(id);
+            db.Ingredients.Remove(ingredients);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
