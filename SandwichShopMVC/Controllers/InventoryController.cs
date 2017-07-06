688d16c32886fa1ba9f4f23005b01174f99e1235
NashNWood <nashua.wood@gmail.com>
2017-07-06 11:42:09 -0700

    Created the Inventory Controller, as well as pushed some mock data to the SandwichShopMVCDb1 Database.

---
+++ SandwichShopMVC/Controllers/InventoryController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SandwichShopMVC.DAL;
+using SandwichShopMVC.Models;
+
+namespace SandwichShopMVC.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private RestaurantContext db = new RestaurantContext();
+
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View(db.Inventory.ToList());
+        }
+
+        // GET: Inventory/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventory/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,InventoryName,Quantity")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Inventory.Add(inventory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(inventory);
+        }
+
+        // GET: Inventory/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,InventoryName,Quantity")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inventory inventory = db.Inventory.Find(id);
+            db.Inventory.Remove(inventory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
